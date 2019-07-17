438b351c1f29f4a4eac8b28f9e2c77d0da7c8a2a
juarejo <jose.juarez@tecnotree.com>
2019-07-17 12:38:15 -0300

    first commit

---
+++ src/main/java/com/producer/controller/TestController.java
@@ -0,0 +1,43 @@
+package com.producer.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.producer.model.Book;
+
+@RestController
+public class TestController {
+	
+	@RequestMapping(value = "/books", method = RequestMethod.GET)
+	@HystrixCommand(fallbackMethod = "getDataFallBack")
+	public Book firstPage() {
+		System.out.println("Inside firstPage");
+		
+		Book book = new Book();
+		book.setDescription("The best book");
+		book.setIsbn("book1");
+		book.setName("The witcher");
+		book.setPrice(2.3);
+		
+		if(book.getName().equalsIgnoreCase("emp1"))
+			throw new RuntimeException();
+		
+		return book;
+	}
+	
+	public Book getDataFallBack() {
+		
+		System.out.println("Inside fallback");
+		Book book = new Book();
+		book.setDescription("fallback-desc");
+		book.setIsbn("fallback-isbn");
+		book.setName("fallback-name");
+		book.setPrice(0);
+
+		return book;
+		
+	}
+}
---
+++ src/main/java/com/producer/model/Book.java
@@ -0,0 +1,41 @@
+package com.producer.model;
+
+public class Book {
+	private String isbn;
+	private String name;
+	private String description;
+	private double price;
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+}
